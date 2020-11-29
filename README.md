ed04487d9c0ff5899990d6c090c1f35c5f53d700
imeepos <1037483576@qq.com>
2020-11-29 13:00:36 +0800

    feature(update readme docs)

--- README.md
+++ README.md
@@ -1,2 +1,26 @@
 # decorator
 @noding/decorator 装饰器定义
+
+
+```ts
+
+import { createDecorator, createClassDecorator, createPropertyDecorator, createMethodDecorator, createParameterDecorator, getDecorator } from '@noding/decorator';
+const DemoClass = createClassDecorator(`demo class`)
+const DemoProperty = createPropertyDecorator(`demo property`)
+const DemoMethod = createMethodDecorator(`demo method`)
+const DemoParameter = createParameterDecorator(`demo parameter`)
+const Demo2Class = createDecorator(`demo2 class`)
+
+@Demo2Class()
+@DemoClass()
+export class UserDemo {
+    @DemoProperty()
+    username: string;
+    @DemoMethod()
+    add(@DemoParameter() a: number, @DemoParameter() b: number): number {
+        return a + b;
+    }
+    constructor(@DemoParameter() a: number, @DemoParameter() b: number) { }
+}
+const clsDemo = getDecorator(UserDemo)
+```
--- src/test.ts
+++ src/test.ts
@@ -1,24 +1,22 @@
 
-import { createDecorator, getDecorator } from './index';
-const DemoClass = createDecorator(`demo class`)
-const DemoProperty = createDecorator(`demo property`)
-const DemoMethod = createDecorator(`demo method`)
-const DemoParameter = createDecorator(`demo parameter`)
+import { createDecorator, createClassDecorator, createPropertyDecorator, createMethodDecorator, createParameterDecorator, getDecorator } from './index';
+const DemoClass = createClassDecorator(`demo class`)
+const DemoProperty = createPropertyDecorator(`demo property`)
+const DemoMethod = createMethodDecorator(`demo method`)
+const DemoParameter = createParameterDecorator(`demo parameter`)
+const Demo2Class = createDecorator(`demo2 class`)
 
+@Demo2Class()
 @DemoClass()
 export class UserDemo {
-
     @DemoProperty()
     username: string;
-
     @DemoMethod()
     add(@DemoParameter() a: number, @DemoParameter() b: number): number {
         return a + b;
     }
-
     constructor(@DemoParameter() a: number, @DemoParameter() b: number) { }
 }
-
 const clsDemo = getDecorator(UserDemo)
 
 debugger;
